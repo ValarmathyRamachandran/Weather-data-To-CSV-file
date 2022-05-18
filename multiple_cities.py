9d293e6858e7250855119c97cefea8cda196bb9a
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-05-18 19:18:49 +0530

    [Valarmathy]:refactored single city code to write the data in correct format into csv file

--- multiple_cities.py
+++ multiple_cities.py
@@ -5,6 +5,8 @@ from urllib.request import urlopen
 header = ['name', 'longitude', 'latitude', 'temperature']
 
 
+
+
 def get_city_data(city_name):
     base_url = 'https://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
     url = base_url + city_name
@@ -13,16 +15,17 @@ def get_city_data(city_name):
     data_json = {"records": json.loads(response.read())}
 
     return data_json
-    # print(data_json)
+    print(data_json)
 
 
 def write_csv(data_json):
-    with open('data.csv', 'w', encoding='utf-8') as file:
+    with open('citydata.csv', 'w', encoding='utf-8', newline='') as file:
         csv_file = csv.writer(file)
         csv_file.writerow(header)
+
         for item in data_json.values():
-            csv_file.writerow([item['name'], item['coord']['lon'], item['coord']['lat'],
-                               item['main']['temp']])
+            csv_file.writerows([item['name'], item['coord']['lon'], item['coord']['lat'],
+                                item['main']['temp']])
 
         return {'msg': 'csv file updated successfully'}
 
@@ -30,6 +33,5 @@ def write_csv(data_json):
 if __name__ == '__main__':
     city = input("Enter the city name :\n")
     data = get_city_data(city)
-    print(data)
-    update = write_csv(data)
-    print(update)
+
+    write_csv(data)
