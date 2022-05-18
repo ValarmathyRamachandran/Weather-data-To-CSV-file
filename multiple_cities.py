50c38df7c38e246504cf23d51b6726c6689fce47
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-05-18 19:55:27 +0530

    [Valarmathy]:added multiple cities using for loop and updated it into csv file

--- multiple_cities.py
+++ multiple_cities.py
@@ -1,37 +1,56 @@
 import csv
 import json
-from urllib.request import urlopen
+import requests
 
-header = ['name', 'longitude', 'latitude', 'temperature']
+city_list = ['Mumbai',
+             'Kolkata',
+             'Delhi',
+             'Chennai',
+             'Bangalore',
+             'Hyderabad',
+             'Kolkata',
+             'Pune',
+             'Lucknow',
+             'Kanpur',
+             'Indore',
+             'Patna',
+             'Chandigarh',
+             'Shimla',
+             'Thiruvananthapuram',
+             'Bhopal',
+             'Mumbai',
+             'Srinagar',
+             'Shillong',
+             'Jaipur'
+
+             ]
+
+header = ['name', 'longitude', 'latitude', 'temperature', 'temp_max', 'temp_min']
 
 
-
-
-def get_city_data(city_name):
+def get_data(city_name):
     base_url = 'https://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
     url = base_url + city_name
-    response = urlopen(url)
+    response = requests.get(url)
 
-    data_json = {"records": json.loads(response.read())}
+    text = response.text
+    json_data = {"record": json.loads(text)}
+    write_data(json_data)
 
-    return data_json
-    print(data_json)
 
-
-def write_csv(data_json):
-    with open('citydata.csv', 'w', encoding='utf-8', newline='') as file:
+def write_data(json_data):
+    with open('multiple_data.csv', 'a', encoding='utf-8') as file:
         csv_file = csv.writer(file)
         csv_file.writerow(header)
+        for data in json_data.values():
+            csv_file.writerow([data['name'], data['coord']['lon'], data['coord']['lat'], data['main']['temp'],
+                               data['main']['temp_max'], data['main']['temp_min']])
 
-        for item in data_json.values():
-            csv_file.writerows([item['name'], item['coord']['lon'], item['coord']['lat'],
-                                item['main']['temp']])
 
-        return {'msg': 'csv file updated successfully'}
+def main(cities):
+    for city in cities:
+        get_data(city)
 
 
 if __name__ == '__main__':
-    city = input("Enter the city name :\n")
-    data = get_city_data(city)
-
-    write_csv(data)
+    main(city_list)
